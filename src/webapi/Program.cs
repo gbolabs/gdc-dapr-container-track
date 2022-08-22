968f899a557e3b94bfe9f13f8a0ce15027fbe918
Gautier Boder <gboder@gmail.com>
2022-08-22 12:41:19 +0000

    wip

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,11 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+const string MyAllowSpecificOrigins = "development";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://stopr114emp001.z1.web.core.windows.net",
+                              "https://energy.isago.ch",
+                                                "http://localhost:4200",
+                                              "http://localhost");
+                      });
+});
 
 var app = builder.Build();
 
@@ -15,11 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseCors();
 app.Run();
