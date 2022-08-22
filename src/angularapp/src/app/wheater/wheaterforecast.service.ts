4f9bc55d069177af33adc4cc4daee32de831e399
Gautier Boder <gboder@gmail.com>
2022-08-22 15:02:18 +0000

    wip

--- src/angularapp/src/app/wheater/wheaterforecast.service.ts
+++ src/angularapp/src/app/wheater/wheaterforecast.service.ts
@@ -16,7 +16,9 @@ export class WheaterforecastService {
   getForecast(): Observable<Weatherforecast> {
 
     // return of<Weatherforecast>(CurrentWeatherforecast);
-
-    return this.http.get<Weatherforecast>("http://127.0.0.1:5244/weatherforecast");
+    var url= "https://func-gcamp2022-codespaces-demo-001.azurewebsites.net/api/func_gcamp2022_codespaces_demo_001?code="
+    var key= "YkYvb5OnamdUs5Q_vpeqTuaO2-Fx7cMjkyPlZwBYNsmuAzFudLe2UQ=="
+    return this.http.get<Weatherforecast>(url+key);
+    // return this.http.get<Weatherforecast>("http://127.0.0.1:5244/weatherforecast");
   }
 }
